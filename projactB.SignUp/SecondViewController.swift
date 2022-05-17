b5eacacc4c76abb56304ab6fdda2b2488fad1953
parkhohyun <haor3@hanmail.net>
2022-05-17 22:22:55 +0900

    projact B  활성화 전...

--- projactB.SignUp/SecondViewController.swift
+++ projactB.SignUp/SecondViewController.swift
@@ -1,54 +1,81 @@
-    //
-    //  SecondViewController.swift
-    //  projactB.SignUp
-    //
-    //  Created by 박호현 on 2022/05/17.
-    //
+//
+//  SecondViewController.swift
+//  projactB.SignUp
+//
+//  Created by 박호현 on 2022/05/17.
+//
 
-    import UIKit
+import UIKit
 
-    class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-        
-        lazy var imagePicker: UIImagePickerController = {
-            let picker: UIImagePickerController = UIImagePickerController()
-            picker.sourceType = .photoLibrary
-            picker.delegate = self
-            return picker
-        }()
-        
-        
-        @IBOutlet weak var imageView: UIImageView!
-        
-        @IBAction func touchUpSelectImageButton(_ sender: UIButton) {
-            self.present(self.imagePicker, animated: true, completion:  nil)
-        }
-        
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            self.dismiss(animated: true, completion: nil)
-        }
-        
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            
-            if let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-                self.imageView.image = originalImage
-            }
-            
-            self.dismiss(animated: true, completion: nil)
-        }
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-        }
-        
-        // @IBAction func popToPrev() {
-        //    self.navigationController?.popViewController(animated: true)
-        
-        @IBAction func dismissModel() {
-            self.dismiss(animated: true, completion: nil)
-        }
-        
-        @IBAction func tapView(_ sender: UITapGestureRecognizer) {
-            self.view.endEditing(true)
-            
+class SecondViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var password2Field: UITextField!
+    
+    //   @IBAction func touchUpsetButton(_ sender: UIButton) {
+    //        UserInformation.shared.name = nameField.text
+    //        UserInformation.shared.passward = passwordField.text
+    //        UserInformation.shared.passward2 = password2Field.text
+    //     }
+    
+    lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        return picker
+    }()
+    
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func touchUpSelectImageButton(_ sender: UIButton) {
+        self.present(self.imagePicker, animated: true, completion:  nil)
+    }
+    
+    // @IBAction func popToPrev() {
+    //    self.navigationController?.popViewController(animated: true)
+    
+    @IBAction func dismissModel() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+}
+
+extension SecondViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        nextButton.isEnabled = false
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if nextButton.isEnabled == true {
+            UserInformation.shared.passward = UserInformation.shared.passward2
         }
     }
+}
+
+extension SecondViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imageView.image = originalImage
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ projactB.SignUp/UserInformation.swift
@@ -0,0 +1,16 @@
+//
+//  UserInformation.swift
+//  projactB.SignUp
+//
+//  Created by 박호현 on 2022/05/17.
+//
+
+import Foundation
+
+class UserInformation {
+    static let shared: UserInformation = UserInformation()
+    
+    var name: String?
+    var passward: String?
+    var passward2: String?
+}
